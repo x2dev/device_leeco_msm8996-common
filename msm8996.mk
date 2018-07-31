3f206b70d3b1799fa7ed39826a4b32041d794a42
shivatejapeddi <shivatejapeddi@gmail.com>
2018-07-31 18:17:50 +0000

    Revert "msm8996: add custom biometrics to fix gdxfingerprint"
    This reverts commit d32214a4f1e81600859eacbd793afb6c4e8071f0.

--- msm8996.mk
+++ msm8996.mk
@@ -219,7 +219,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.0-service-custom \
+    android.hardware.biometrics.fingerprint@2.0-service \
     fingerprintd \
     fingerprint.msm8996
 
